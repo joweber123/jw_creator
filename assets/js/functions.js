857b578606b83f4f04ba8d64535b56c49f5737f6
James Weber <joweber123@gmail.com>
2017-07-22 13:43:02 +0900

    First commit

---
+++ assets/js/functions.js
@@ -0,0 +1,19 @@
+$( document ).ready(function() {
+  smoothScroll(500);
+  // Get started!
+
+});
+
+function smoothScroll (duration) {
+	$('a[href^="#"]').on('click', function(event) {
+
+	    var target = $( $(this).attr('href') );
+
+	    if( target.length ) {
+	        event.preventDefault();
+	        $('html, body').animate({
+	            scrollTop: target.offset().top
+	        }, duration);
+	    }
+	});
+}
