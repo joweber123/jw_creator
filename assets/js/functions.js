e5af248f8c5d698a7dc1c48b0772535e2906ba51
James Weber <joweber123@gmail.com>
2017-12-20 18:08:01 +0900

    added language functionality

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -6,3 +6,20 @@ $(window).load(function() {
 $(window).on("orientationchange",function() {
   location.reload();
 });
+
+$('.language-selector .language-selector-child').on('click',function(){
+   var eq = $(this).index();
+   $('.about-description').removeClass('show');
+   $('.about-description').eq(eq).addClass('show');
+	 $('.bio-more-child').removeClass('show');
+   $('.bio-more-child').eq(eq).addClass('show');
+
+
+});
+$('.bio-more-button').on('click',function(){
+   $('.bio-more-container').toggle(200);
+
+
+
+
+});
